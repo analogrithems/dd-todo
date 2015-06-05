320bc921fcf049aef27463b4c6e58cbc6ad56275
root <root@vault.asynonymous.net>
2015-06-04 23:53:13 -1000

    updated readme and index, linked the dd library

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dd-todo
-todo demo using dd
+Simple demo of the Todo app written with [dd](https://github.com/drdelambre/drdelambre)
+
+Also uses a [HTML 5 + Bootstrap Boiler Plate](http://www.initializr.com/)
--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+(function() {
 
+	var Task = function(data){
+	    var self = $dd.model({
+	        name: '',
+	        complete: false
+	    }).on_fill(function(_data){
+	        if(_data.name){
+	            var _name = _data.name.toLowerCase();
+	            _data.name = _name.charAt(0).toUpperCase() + _name.slice(1);
+	        }
+
+	    });
+	
+	    return self.fill(data);
+	};	
+
+    $dd.ioc.register('model.Task',function(){
+        return Task;
+    });
+}());
